8ec68fc5ad7d08c2359dcec065c9e004b56ccbcc
Dietrich Epp <depp@zdome.net>
2013-04-27 03:43:54 -0700

    Add sprites for player and enemy

--- js/battlescreen.js
+++ js/battlescreen.js
@@ -9,6 +9,15 @@ function BattleScreen() {
     ]
     this.menu = new Menu(items, this.sprites, this.font, 16, 16*17, 16*5);
     this.setBackground('mountain');
+
+    this.party = [
+	{ 'sprite': 'player_battle',
+	  'pos': [485, 195] }
+    ];
+    this.enemy = [
+	{ 'sprite': 'gremlin',
+	  'pos': [82, 190] }
+    ];
 }
 
 BattleScreen.prototype.setBackground = function (name) {
@@ -25,6 +34,14 @@ BattleScreen.prototype.draw = function() {
     var cxt = main.cxt;
     cxt.drawImage(self.background, 0, 0);
     this.menu.draw();
+    for (var i = 0; i < this.party.length; i++) {
+	var c = this.party[i];
+	this.sprites.draw(c.pos[0], c.pos[1], c.sprite, 1);
+    }
+    for(var i = 0; i < this.enemy.length; i++) {
+	var c = this.enemy[i];
+	this.sprites.draw(c.pos[0], c.pos[1], c.sprite, 1);
+    }
 }
 
 BattleScreen.prototype.keydown = function(code) {
