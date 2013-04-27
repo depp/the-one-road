1558012a1b0f05aef713ba2010ef1672a35a4a58
Dietrich Epp <depp@zdome.net>
2013-04-27 09:57:06 -0700

    Combine damage number functionality

--- js/battlescreen.js
+++ js/battlescreen.js
@@ -28,7 +28,8 @@ function BattleScreen() {
     this.menu = new Menu(this, items, this.sprites,
 			 this.font, 16, 16*17, 16*5);
     this.setBackground('mountain');
-    this.animation = [];
+    this.animations = [];
+    this.animation_queue = []
 
     this.sprite = {
 	'player': {
@@ -60,20 +61,42 @@ BattleScreen.prototype.setBackground = function(name) {
 
 BattleScreen.prototype.update = function() {
     this.frame++;
-    if (this.animation.length > 0) {
-	var frame = this.frame - this.animation_start;
-	if (this.animation[0].call(this, frame)) {
-	    this.animation.shift()
-	    this.animation_start = this.frame;
+    if (this.animations.length > 0) {
+	var changed = false;
+	for (var i = 0; i < this.animations.length; i++) {
+	    var anim = this.animations[i];
+	    var frame = this.frame - anim.start;
+	    if (anim.funcs[0].call(this, frame)) {
+		anim.funcs.shift();
+		anim.start = this.frame;
+		if (!anim.funcs.length)
+		    changed = true;
+	    }
+	}
+	if (changed) {
+	    var nanims = [];
+	    for (var i = 0; i < this.animations.length; i++) {
+		var anim = this.animations[i];
+		if (anim.funcs.length)
+		    nanims.push(anim);
+	    }
+	    this.animations = nanims;
+	    if (!nanims.length && this.animation_queue.length) {
+		this.animate(this.animation_queue.shift())
+	    }
 	}
     }
 }
 
-BattleScreen.prototype.animate = function() {
-    if (!this.animation.length)
-	this.animation_start = this.frame;
-    for (var i = 0; i < arguments.length; i++)
-	this.animation.push(arguments[i]);
+BattleScreen.prototype.animate = function(queue, funcs) {
+    if (queue) {
+	this.animation_queue.push(funcs);
+    } else {
+	this.animations.push({
+	    'start': this.frame,
+	    'funcs': funcs
+	})
+    }
 }
 
 BattleScreen.prototype.draw = function() {
@@ -99,7 +122,7 @@ BattleScreen.prototype.draw = function() {
 }
 
 BattleScreen.prototype.keydown = function(code) {
-    if (this.menu && !this.animation.length)
+    if (this.menu && !this.animations.length)
 	this.menu.keydown(code);
 }
 
@@ -134,6 +157,31 @@ function battle_interp(p1, p2, t, type) {
     return [x, y];
 }
 
+BattleScreen.prototype.do_damage = function(target, amt) {
+    var tpos = this.sprite[target].pos;
+    var text;
+    if (amt > 0)
+	text = '+' + amt.toString();
+    else if (amt < 0)
+	text = amt.toString();
+    else
+	return;
+    this.sprite.stat.sprite = new BSText(text);
+    this.animate(false, [
+	function(frame) {
+	    this.sprite.effect.sprite = null;
+	    var t = frame > 10 ? 1 : frame / 10;
+	    this.sprite.stat.pos = [
+		tpos[0], tpos[1] - 12 * t];
+	    return frame >= 20;
+	},
+	function(frame) {
+	    this.sprite.stat.sprite = null;
+	    return true;
+	}
+    ])
+}
+
 BattleScreen.prototype.do_attack = function(actor, target) {
     var apos = this.sprite[actor].pos;
     var tpos = this.sprite[target].pos;
@@ -141,7 +189,7 @@ BattleScreen.prototype.do_attack = function(actor, target) {
 	tpos[0] + (apos[0] < tpos[0] ? -40 : +40),
 	tpos[1]
     ]
-    this.animate(
+    this.animate(false, [
 	function(frame) {
 	    var t = frame / 30;
 	    t = battle_smooth(t);
@@ -149,12 +197,8 @@ BattleScreen.prototype.do_attack = function(actor, target) {
 	    return frame >= 30;
 	},
 	function(frame) {
-	    var t = frame / 10;
-	    if (!this.sprite['stat'].sprite)
-		this.sprite['stat'].sprite = new BSText('-50')
-	    this.sprite['stat'].pos = [
-		tpos[0], tpos[1] - 12 * t];
-	    return frame >= 10;
+	    this.do_damage(target, -50);
+	    return true;
 	},
 	function(frame) {
 	    var t = battle_smooth(frame / 10);
@@ -162,11 +206,10 @@ BattleScreen.prototype.do_attack = function(actor, target) {
 	    return frame >= 10;
 	},
 	function(frame) {
-	    this.sprite['stat'].sprite = null;
 	    this.sprite[actor].pos = apos;
 	    return true;
 	}
-    )
+    ])
 }
 
 BattleScreen.prototype.do_spell = function(actor, target) {
@@ -178,7 +221,7 @@ BattleScreen.prototype.do_spell = function(actor, target) {
 	spos[0] += 16;
     else
 	spos[0] -= 16;
-    this.animate(
+    this.animate(false, [
 	function(frame) {
 	    if (!this.sprite.effect.sprite)
 		this.sprite.effect.sprite = new BSSprite('fire_1');
@@ -198,19 +241,10 @@ BattleScreen.prototype.do_spell = function(actor, target) {
 	    return frame >= 20;
 	},
 	function(frame) {
-	    this.sprite.effect.sprite = null;
-	    var t = frame > 10 ? 1 : frame / 10;
-	    if (!this.sprite.stat.sprite)
-		this.sprite.stat.sprite = new BSText('-50')
-	    this.sprite.stat.pos = [
-		tpos[0], tpos[1] - 12 * t];
-	    return frame >= 20;
-	},
-	function(frame) {
-	    this.sprite.stat.sprite = null;
+	    this.do_damage(target, -50);
 	    return true;
 	}
-    )
+    ])
 }
 
 BattleScreen.prototype.act_attack = function() {
