2e14db83daf0f1cc24bc968f73afd4e60c639a7d
Dietrich Epp <depp@zdome.net>
2013-04-28 13:11:51 -0700

    Add SFX playback

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,6 @@
 var audio_files = {};
 var music_track = null;
+var sfx_file = null;
 
 function audio_obj(path) {
     if (path in audio_files) {
@@ -38,4 +39,34 @@ function music_play(track) {
     music_track = obj;
 }
 
-music_play('audio/background');
+var sfx_end = null;
+var sfx_queue = null;
+
+function audio_timer() {
+    if (sfx_end && sfx_file.currentTime >= sfx_end)
+	sfx_file.pause();
+}
+
+function sfxPlay2() {
+    var name = sfx_queue;
+    sfx_queue = null;
+    sfxPlay(name);
+}
+
+function sfxPlay(name) {
+    if (!sfx_file || sfx_queue) {
+	sfx_queue = name;
+	if (!sfx_file) {
+	    sfx_file = audio_obj('audio/sfx');
+	    sfx_file.addEventListener('timeupdate', audio_timer, false);
+	    sfx_file.addEventListener('canplaythrough', sfxPlay2, false);
+	}
+    } else {
+	var info = JSON_sfx[name];
+	if (!sfx_file.paused)
+	    sfx_file.pause();
+	sfx_file.currentTime = info[0];
+	sfx_end = info[1];
+	sfx_file.play();
+    }
+}
